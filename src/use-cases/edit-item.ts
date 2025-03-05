c606d26848b4e6b6c1a9de23425ff4f2cfbe4f64
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-03-05 18:25:27 -0300

    feat: implements edit item use-case

---
+++ src/use-cases/edit-item.ts
@@ -0,0 +1,39 @@
+import { db } from "@/lib/database/drizzle";
+import { guildTable, itemTable } from "@/lib/database/schema";
+import { and, eq } from "drizzle-orm";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+
+interface EditItemProps {
+	id: number;
+	name: string;
+	description: string;
+	url: string;
+	guildExternalId: string;
+}
+
+export async function editItemUseCase({
+	id,
+	name,
+	description,
+	guildExternalId,
+	url,
+}: EditItemProps) {
+	const guild = await db
+		.select()
+		.from(guildTable)
+		.where(eq(guildTable.externalId, guildExternalId))
+		.get();
+
+	if (!guild) {
+		throw new ResourceNotFoundError();
+	}
+
+	await db
+		.update(itemTable)
+		.set({
+			name,
+			description,
+			url,
+		})
+		.where(and(eq(itemTable.id, id), eq(itemTable.guildId, guild.id)));
+}
