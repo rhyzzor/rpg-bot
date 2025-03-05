22eaf5de4b95dd4f8a8b5cef673017f69705bfc6
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-03-05 09:20:37 -0300

    feat: connect to the discord bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Buns!");
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import { env } from "./env";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (readyClient) => {
+	console.log("Ready! Logged in as ", readyClient.user.tag);
+});
+
+client.login(env.DISCORD_TOKEN);
+
+console.log(env);
