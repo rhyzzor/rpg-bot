5c96d335d176b6c38f1c26bcefd8b86b8e1da2ed
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-03-05 10:05:47 -0300

    feat: implements a function to deploy commands on guild creation and add a listener to event interaction

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,37 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
+import { commands } from "./commands";
+import { deployCommands } from "./commands/deploy";
 import { env } from "./env";
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
+});
 
 client.once(Events.ClientReady, (readyClient) => {
 	console.log("Ready! Logged in as ", readyClient.user.tag);
+});
+
+client.on(Events.GuildCreate, async (guild) => {
+	await deployCommands({ guildId: guild.id });
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isCommand()) {
+		return;
+	}
+
+	const { commandName } = interaction;
+
+	const selectedCommand = commands[commandName as keyof typeof commands];
+
+	if (selectedCommand) {
+		selectedCommand.execute(interaction);
+	}
 });
 
 client.login(env.DISCORD_TOKEN);
