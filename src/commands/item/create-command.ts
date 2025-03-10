4326ea385e528ac49351f9ed6c61780a12d1365b
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-03-10 19:40:38 -0300

    refactor: remove delete command and add i18n to create command

--- src/commands/item/create-command.ts
+++ src/commands/item/create-command.ts
@@ -1,50 +1,56 @@
+import { generateItemModal } from "@/components/item/modal-builder";
 import { createItemUseCase } from "@/use-cases/create-item";
-import { generateItemModal } from "@/utils/modal-builder";
-import {} from "@discordjs/builders";
-import { type CommandInteraction, SlashCommandBuilder } from "discord.js";
+import type { CommandOptions, SlashCommandProps } from "commandkit";
+import { SlashCommandBuilder } from "discord.js";
 
 export const data = new SlashCommandBuilder()
 	.setName("criar-item")
-	.setDescription("Cria um novo item");
+	.setDescription("Cria um item")
+	.setNameLocalizations({
+		"pt-BR": "criar-item",
+		"en-US": "create-item",
+	})
+	.setDescriptionLocalizations({
+		"pt-BR": "Cria um item",
+		"en-US": "Create an item",
+	});
 
-export async function execute(interaction: CommandInteraction) {
-	try {
-		const customId = `create-item-modal.${interaction.id}`;
+export async function run({ interaction }: SlashCommandProps) {
+	const customId = interaction.id;
 
-		const modal = generateItemModal({ customId, title: "Criar um novo item" });
+	const modal = generateItemModal({ customId, title: "Criar um novo item" });
 
-		await interaction.showModal(modal);
+	await interaction.showModal(modal);
 
-		interaction
-			.awaitModalSubmit({
-				filter: (i) => i.customId === customId,
-				time: 60000,
-			})
-			.then(async (modalInteraction) => {
-				if (!modalInteraction.guildId) {
-					return await modalInteraction.reply("Não foi possível criar o item");
-				}
+	const modalInteraction = await interaction.awaitModalSubmit({
+		filter: (i) => i.customId === customId && i.user.id === interaction.user.id,
+		time: 60_000,
+	});
 
-				const getValue = (field: string) =>
-					modalInteraction.fields.getTextInputValue(field);
-
-				const itemName = getValue("itemNameInput");
-				const descriptionName = getValue("itemDescriptionInput");
-				const imageUrl = getValue("itemUrlInput");
-
-				await createItemUseCase({
-					description: descriptionName,
-					guildExternalId: modalInteraction.guildId,
-					name: itemName,
-					url: imageUrl,
-				});
-
-				return await modalInteraction.reply({
-					content: `Item **${itemName}** criado com sucesso!`,
-					flags: "Ephemeral",
-				});
-			});
-	} catch (error) {
-		console.error(error);
+	if (!modalInteraction.guildId) {
+		return await modalInteraction.reply("Não foi possível criar o item");
 	}
+
+	const getValue = (field: string) =>
+		modalInteraction.fields.getTextInputValue(field);
+
+	const itemName = getValue("itemNameInput");
+	const descriptionName = getValue("itemDescriptionInput");
+	const imageUrl = getValue("itemUrlInput");
+
+	await createItemUseCase({
+		description: descriptionName,
+		guildExternalId: modalInteraction.guildId,
+		name: itemName,
+		url: imageUrl,
+	});
+
+	return await modalInteraction.reply({
+		content: `Item **${itemName}** criado com sucesso!`,
+		flags: "Ephemeral",
+	});
 }
+
+export const options: CommandOptions = {
+	devOnly: true,
+};
