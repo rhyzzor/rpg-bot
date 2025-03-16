ae144e059fdefa196dabc26e473a5650c1f449d2
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-03-16 18:05:57 -0300

    feat: add a command to send points to player

---
+++ src/commands/player/admin/send-point-command.ts
@@ -0,0 +1,76 @@
+import { translate } from "@/lib/i18n";
+import { listPlayersUseCase } from "@/use-cases/list-players";
+import { sendPointUseCase } from "@/use-cases/send-point";
+import type {
+	AutocompleteProps,
+	CommandOptions,
+	SlashCommandProps,
+} from "commandkit";
+import { SlashCommandBuilder } from "discord.js";
+
+export const data = new SlashCommandBuilder()
+	.setName("send-point")
+	.setNameLocalizations({ "pt-BR": "enviar-ponto" })
+	.setDescription("Send points to another player")
+	.setDescriptionLocalizations({ "pt-BR": "Envia pontos para outro jogador" })
+	.addIntegerOption((option) =>
+		option
+			.setName("sheet")
+			.setNameLocalizations({ "pt-BR": "ficha" })
+			.setDescription("Player/Extra Character Sheet")
+			.setDescriptionLocalizations({
+				"pt-BR": "Ficha do Jogador/Personagem Extra",
+			})
+			.setAutocomplete(true)
+			.setRequired(true),
+	)
+	.addIntegerOption((option) =>
+		option
+			.setName("quantity")
+			.setNameLocalizations({ "pt-BR": "quantidade" })
+			.setDescription("Quantity")
+			.setDescriptionLocalizations({ "pt-BR": "Quantidade" })
+			.setRequired(true),
+	);
+
+export async function run({ interaction }: SlashCommandProps) {
+	if (!interaction.guildId) return;
+
+	const guildId = interaction.guildId;
+
+	const playerId = interaction.options.getInteger("sheet", true);
+	const quantity = interaction.options.getInteger("quantity", true);
+
+	await sendPointUseCase({ guildId, playerId, quantity });
+
+	return await interaction.reply({
+		content: translate("player.points.send", interaction.locale),
+		flags: "Ephemeral",
+	});
+}
+
+export async function autocomplete({ interaction }: AutocompleteProps) {
+	if (!interaction.guildId) return;
+
+	const guildId = interaction.guildId;
+
+	const focusedOption = interaction.options.getFocused(true);
+
+	const sheets = await listPlayersUseCase({ guildId });
+
+	const filtered = sheets
+		.filter((sheet) =>
+			sheet.name.toLowerCase().startsWith(focusedOption.value.toLowerCase()),
+		)
+		.map((sheet) => ({
+			name: sheet.name,
+			value: sheet.id,
+		}));
+
+	await interaction.respond(filtered);
+}
+
+export const options: CommandOptions = {
+	devOnly: true,
+	userPermissions: ["Administrator"],
+};
---
+++ src/use-cases/send-point.ts
@@ -0,0 +1,22 @@
+import { db } from "@/lib/database/drizzle";
+import { playerTable } from "@/lib/database/schema";
+import { and, eq, sql } from "drizzle-orm";
+
+interface SendPointProps {
+	guildId: string;
+	playerId: number;
+	quantity: number;
+}
+
+export async function sendPointUseCase({
+	guildId,
+	playerId,
+	quantity,
+}: SendPointProps) {
+	await db
+		.update(playerTable)
+		.set({
+			points: sql`${playerTable.points} + ${quantity}`,
+		})
+		.where(and(eq(playerTable.id, playerId), eq(playerTable.guildId, guildId)));
+}
