49bc79b5038e8afe7d1f16efd3590c02ee9c625a
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-03-17 11:20:16 -0300

    chore: add schemas to drizzle config

--- src/lib/database/drizzle.ts
+++ src/lib/database/drizzle.ts
@@ -1,10 +1,11 @@
 import { env } from "@/env";
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
+import * as schema from "./schema";
 
 const turso = createClient({
 	url: env.TURSO_DATABASE_URL,
 	authToken: env.TURSO_AUTH_TOKEN,
 });
 
-export const db = drizzle(turso);
+export const db = drizzle(turso, { schema });
