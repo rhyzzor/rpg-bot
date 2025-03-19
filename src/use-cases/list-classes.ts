226e55b6b7c64c658e0e94bd3be9233ce5783282
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-03-19 10:25:02 -0300

    refactor: add guild id filter

--- src/use-cases/list-classes.ts
+++ src/use-cases/list-classes.ts
@@ -1,12 +1,16 @@
 import { db } from "@/lib/database/drizzle";
 import { classTable } from "@/lib/database/schema";
+import { eq } from "drizzle-orm";
 
 interface ListClassesProps {
 	guildId: string;
 }
 
 export async function listClassesUseCase({ guildId }: ListClassesProps) {
-	const classes = await db.select().from(classTable);
+	const classes = await db
+		.select()
+		.from(classTable)
+		.where(eq(classTable.guildId, guildId));
 
 	//TODO: business rules
 
